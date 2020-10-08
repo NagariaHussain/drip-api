3fa491e31253332893e272af63a545e39bee1c2b
NagariaHussain <hussainbhaitech@gmail.com>
2020-10-08 17:04:17 +0530

    Add api routing and testing

--- app.py
+++ app.py
@@ -3,33 +3,125 @@ from data_processor import DataProcessor
 from drip_calculator import DRIPCalculator
 
 # Flask
-from flask import Flask, request, abort
+from flask import Flask, request, abort, Response, jsonify
 
 # Create flask app
 app = Flask(__name__)
 
 
-df = DataFetcher("MMM")
-dp = DataProcessor(df.get_drip_data(), df.get_stock_data())
-dc = DRIPCalculator(dp)
+# df = DataFetcher("MMM")
+# dp = DataProcessor(df.get_drip_data(), df.get_stock_data())
+# dc = DRIPCalculator(dp)
 
-# Generate report
-dc.generate_projection(1500, 0, False, 0, 5)
+# # Generate report
+# dc.generate_projection(1500, 0, False, 0, 5)
 
-# Print projected data
-print(dc.get_projection_data())
+# # Print projected data
+# print(dc.get_projection_data())
 
 
 @app.route("/api")
 def handle_api_req():
-    print("Start Balance: ", request.args.get("start_balance"))
-    print("Tax Percent: ", request.args.get("tax_percent"))
-    print("Symbol: ", request.args.get("symbol"))
-    print("Reinvested: ", request.args.get("reinvested"))
-    print("Years:", request.args.get("n_years"))
-    print("Employee Contrib", request.args.get("emp_contrib"))
+    # Evaluate start balance
+    start_balance = request.args.get("start_balance")
 
-    return "Hello"
+    if not start_balance:
+        # Start balance not provided
+        abort(404, description="start_balance must be provided")
+    else:
+        try:
+            start_balance = float(start_balance)
+        except ValueError as ve:
+            # Invalid start balance value
+            abort(404, description="invalid start_balance value")
+
+    # Access security symbol
+    symbol = request.args.get("symbol")
+
+    # If symbol is not provided
+    if not symbol:
+        abort(404, description="symbol must be provided")
+    
+    # Access number of years
+    n_years = request.args.get("n_years")
+
+    # If n_years not provided
+    if not n_years:
+        abort(404, description="n_years must be provided")
+    else:
+        try:
+            n_years = int(n_years)
+
+            # Number of years must be greater than 0
+            if n_years <= 0:
+                abort(404, description="n_years should be positive integer")
+        except ValueError as ve:
+            # Invalid value for number of years
+            abort(404, description="invalid n_years value")
+
+    # Access tax percentage value
+    tax_percent = request.args.get("tax_percent")
+
+    if not tax_percent:
+        # Default value is 0
+        tax_percent = 0
+    else:
+        try:
+            tax_percent = float(tax_percent)
+        except ValueError as ve:
+            # Invalid value for tax percent
+            abort(404, description="invaid tax_percent value")
+
+    # Access reinvested boolean
+    reinvested = request.args.get("reinvested")
+
+    # If reinvested in not provided
+    if not reinvested:
+        # Default value is True
+        reinvested = True
+    else:
+        # If reinvested is true
+        if reinvested == "true":
+            reinvested = True
+        # If reinvested is false
+        elif reinvested == "false":
+            reinvested = False
+        # If any other value is provided
+        else:
+            abort(404, "invalid reinvested value, should be either true or false")
+
+    emp_contrib = request.args.get("emp_contrib")
+
+    # If employee contribution is not given
+    if not emp_contrib:
+        # Use default value
+        emp_contrib = 0
+    else:
+        # Try to convert the value to float
+        try:
+            emp_contrib = float(emp_contrib)
+        except ValueError as ve:
+            abort(404, "invalid emp_contrib value")
+
+    # Perform the calculation
+    df = DataFetcher(symbol)
+    dp = DataProcessor(df.get_drip_data(), df.get_stock_data())
+    dc = DRIPCalculator(dp)
+
+    # Generate projections based on given data
+    dc.generate_projection(start_balance, emp_contrib, reinvested, tax_percent, n_years)
+
+    # Get projection data
+    data = dc.get_projection_data()
+
+    # Return data as json
+    return jsonify(data)
+
+
+# Return errors as JSON
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
 
 if __name__ == "__main__":
     app.run()
--- testing.py
+++ testing.py
@@ -1,8 +1,5 @@
+# For making API calls
 import requests
-
-
-
-# Test both the bussiness logic and API
 
 # TEST CASES:
 # 1. Reinvested and Taxable
@@ -12,6 +9,96 @@ import requests
 # API Testing ------------------
 URL = "http://127.0.0.1:5000/api?start_balance=5000&tax_percent=9.8&reinvested=true&symbol=MMM&n_years=10"
 res = requests.get(URL)
+assert res.status_code == 200
+print(res.json())
 
 URL = "http://127.0.0.1:5000/api?symbol=IBM&start_balance=1500&reinvested=false&tax_percent=10&n_years=5"
 res = requests.get(URL)
+print(res.json())
+assert res.status_code == 200
+
+
+# Start balance not provided
+URL = "http://127.0.0.1:5000/api?symbol=IBM&reinvested=false&tax_percent=10&n_years=5"
+res = requests.get(URL)
+
+assert res.status_code == 404
+assert res.json()['error'] == '404 Not Found: start_balance must be provided'
+# -----------------------------
+
+# Invalid start balance
+URL = "http://127.0.0.1:5000/api?start_balance=hello&symbol=IBM&reinvested=false&tax_percent=10&n_years=5"
+res = requests.get(URL)
+
+assert res.status_code == 404
+assert res.json()['error'] == '404 Not Found: invalid start_balance value'
+# -----------------------------
+
+# Symbol not provided
+URL = "http://127.0.0.1:5000/api?start_balance=15000&reinvested=false&tax_percent=10&n_years=5"
+res = requests.get(URL)
+
+assert res.status_code == 404
+assert res.json()['error'] == '404 Not Found: symbol must be provided'
+
+# -----------------------------
+
+
+# Number of years not provided
+URL = "http://127.0.0.1:5000/api?start_balance=15000&symbol=MMM&reinvested=false&tax_percent=10"
+res = requests.get(URL)
+
+assert res.status_code == 404
+assert res.json()['error'] == '404 Not Found: n_years must be provided'
+
+# -----------------------------
+
+# Invalid number of years provided
+URL = "http://127.0.0.1:5000/api?start_balance=15000&reinvested=false&symbol=IBM&tax_percent=10&n_years=hello"
+res = requests.get(URL)
+
+assert res.status_code == 404
+assert res.json()['error'] == '404 Not Found: invalid n_years value'
+
+# -----------------------------
+
+# Negative Number of years
+
+URL = "http://127.0.0.1:5000/api?n_years=-1&start_balance=15000&symbol=MMM&reinvested=false&tax_percent=10"
+res = requests.get(URL)
+
+assert res.status_code == 404
+assert res.json()['error'] == '404 Not Found: n_years should be positive integer'
+
+# -----------------------------
+
+# Invalid tax percent value
+
+URL = "http://127.0.0.1:5000/api?n_years=5&start_balance=15000&symbol=MMM&reinvested=false&tax_percent=hello"
+res = requests.get(URL)
+
+assert res.status_code == 404
+assert res.json()['error'] == '404 Not Found: invaid tax_percent value'
+
+# -----------------------------
+
+# Invalid reinvested value
+URL = "http://127.0.0.1:5000/api?n_years=5&start_balance=15000&symbol=MMM&reinvested=hello"
+res = requests.get(URL)
+
+assert res.status_code == 404
+assert res.json()['error'] == '404 Not Found: invalid reinvested value, should be either true or false'
+
+# -----------------------------
+
+# Invalid emp contrib value
+URL = "http://127.0.0.1:5000/api?n_years=5&start_balance=15000&symbol=MMM&reinvested=true&emp_contrib=hello"
+res = requests.get(URL)
+
+assert res.status_code == 404
+assert res.json()['error'] == '404 Not Found: invalid emp_contrib value'
+
+# -----------------------------
+
+
+print("ALL tests passed!")
